0cdd1d5ff1cf601ae198fc593b31a541a83e12c9
solomonayele <abc>
2022-12-10 19:56:29 -0600

    lab 10 part 2 sorry

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div id="app">
+
+  <h1>Would you rather.......</h1>
+
+  <would-you-rather
+      v-for="question in questions"
+      v-bind:id="question.id"
+      v-bind:question="question.question"
+      v-bind:answer1="question.answer1"
+      v-bind:answer2="question.answer2"
+      v-on:answer-changed="answerChanged"></would-you-rather>
+
+
+
+  <h2>Which one would you pick? </h2>
+
+  <p v-for="choice in choices"> {{choice}}</p>
+
+   </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: 'Would you rather play soccer or football ',
+          answer1: 'soccer',
+          answer2: 'football',
+
+        },
+        {
+          id: 1,
+          question: 'what is you favorite food ',
+          answer1: 'pizza',
+          answer2: 'burger'
+        },
+        {
+          id: 2,
+          question: 'what is you favorite class you took ',
+          answer1: 'Jave',
+          answer2: 'web devloper'
+
+        },
+      ],
+      choices: []
+
+    }
+  },
+  methods: {
+
+
+    answerChanged (id, choice) {
+      this.choices[id] = choice
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: #8104f5
+ }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #04fad0;
   margin-top: 60px;
+  background: #00050c
 }
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="wyr">
+
+<h2> Please make your choice! </h2>
+
+  <h3 v-bind:value="question"> {{ question }} </h3>
+
+   <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+<label>{{answer1}}</label>
+
+ <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade" >
+<label>{{answer2}}</label>
+
+
+ </div>
+
+   <div>
+ <h4> {{question2}}</h4>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    id: Number,
+    question: String,
+    answer1: String,
+    answer2: String,
+
+
+  },
+  data () {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+
+      this.$emit('answer-changed', this.id, this.choice)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+h3 {
+margin: 40px 0 0;
+}
+
+.wyr {
+border: 2px #09ecc7 solid;
+  background: black;
+  margin: 10px;
+}
+</style>
